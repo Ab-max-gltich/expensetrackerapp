a076c7705c750df05418e562b29464c9585ad21d
Abdur Rahmaan Hassan <73424154+Ab-max-gltich@users.noreply.github.com>
2023-02-06 00:07:19 +0100

    Code Explained
    Explanation line by line

--- app/src/main/java/com/example/expensetrackerapp/ExpenseDbHelper.kt
+++ app/src/main/java/com/example/expensetrackerapp/ExpenseDbHelper.kt
@@ -6,8 +6,13 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 
+/*
+ * The class of ExpenseDbHelper had external supervision and guidance because of inadequate
+ * knowledge of SQlite
+ */
+//ExpenseDbHelper: extends the SQLiteOpenHelper, used to create a local database to save the Expenses
 class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
-
+    //defines values that will be used in creation of the database tables
     companion object {
         private const val DATABASE_NAME = "expenses.db"
         private const val DATABASE_VERSION = 1
@@ -20,7 +25,7 @@ class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     }
 
 
-
+    //onCreate: runs on the startup of the class
     override fun onCreate(db: SQLiteDatabase) {
         // create the "expenses" table
         val createTableSql = "CREATE TABLE $TABLE_NAME (" +
@@ -34,19 +39,23 @@ class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
 
 
     }
-
+    //onUpgrade: used when the database needs to be upgraded. The implementation should use this method to drop tables, add tables,
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
 
     }
 
     // insert a new expense into the database
     fun insertExpense(expense: Expense): Long {
+        //try method to catch any unexpected exceptions while inserting new data
         try {
+            //initializes a writable and readable database
             val db = writableDatabase
             val contentValues = ContentValues()
+            //retrieves the information
             contentValues.put(COLUMN_NAME, expense.name)
             contentValues.put(COLUMN_AMOUNT, expense.amount)
             contentValues.put(COLUMN_DATE, expense.date)
+            //adds the inserteddata into the databse
             return db.insert(TABLE_NAME, null, contentValues)
         } catch (e: Exception) {
             Log.e("ExpenseDbHelper", "Error inserting expense: $e")
@@ -60,6 +69,7 @@ class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         val expenses = ArrayList<Expense>()
         val db = readableDatabase
         val cursor = db.rawQuery("SELECT name, amount, expenseDate FROM $TABLE_NAME", null)
+        //uses the curser value to target specific table elements
         if (cursor.moveToFirst()) {
             do {
                 val id = cursor.getLong(cursor.getColumnIndex(COLUMN_ID))
@@ -67,9 +77,11 @@ class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
                 val amount = cursor.getDouble(cursor.getColumnIndex(COLUMN_AMOUNT))
                 val date = cursor.getString(cursor.getColumnIndex(COLUMN_DATE))
                 expenses.add(Expense(id, name, amount, date))
+                //logs the information for debugging
                 Log.d("Expense", "id: $id, name: $name, amount: $amount, date: $date")
             } while (cursor.moveToNext())
         }
+        //closes the cursor to avoid leaks
         cursor.close()
         return expenses
     }
@@ -78,20 +90,15 @@ class ExpenseDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     // get the total amount spent on expenses
     fun getTotalAmountSpent(): Double {
         val db = readableDatabase
+        //queries the database to find the amount of a expense
         val cursor = db.rawQuery("SELECT SUM($COLUMN_AMOUNT) FROM $TABLE_NAME", null)
+        //moves the cursor to the first value
         cursor.moveToFirst()
+        //starts adding the amount to find the total amount
         val sum = cursor.getDouble(0)
         cursor.close()
+        //return the total Amount of Expenses
         return sum
-    }
-
-    fun updateExpense(expense: Expense): Int {
-        val db = writableDatabase
-        val contentValues = ContentValues()
-        contentValues.put(COLUMN_NAME, expense.name)
-        contentValues.put(COLUMN_AMOUNT, expense.amount)
-        contentValues.put(COLUMN_DATE, expense.date)
-        return db.update(TABLE_NAME, contentValues, "$COLUMN_ID = ?", arrayOf(expense.id.toString()))
     }
 
 
--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -9,26 +9,33 @@ import android.widget.ListView
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
-
+/*
+ * The entirety of MainActivity class has been written by me with minimal help from documentation to
+ * utilize imported functions correctly
+ */
 class MainActivity : Activity() {
 
+    //Late Initializing the Expense Database
     private lateinit var expenseDbHelper: ExpenseDbHelper
     private var listAdapter: ArrayAdapter<Expense>? = null
 
+    //OnCreate function:  responsible for starting p a new instance of the MainActivity app
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //selects a display layout
         setContentView(R.layout.activity_main)
-
+        // creates an instance of the ExpenseDbHelper var
         expenseDbHelper = ExpenseDbHelper(this)
-
-        val expenseDbHelper = ExpenseDbHelper(this)
+        //opens a writable and readable SQLite Databse
         expenseDbHelper.writableDatabase
-
+        //gets all expenses and saves it into list expenses
         val expenses = expenseDbHelper.getAllExpenses()
-
+        //creates a listAdapter using the SimpleDataFormat layout and expenses as resource
         listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, expenses)
         val listView = findViewById<ListView>(R.id.expense_list)
+        //sets the ListView.adaptor as the before-mentioned ListAdapter
         listView.adapter = listAdapter
+        //finds View by Id and sets the format of the total amount display text
         val totalAmountTextView = findViewById<TextView>(R.id.total_amount)
         val totalAmount = expenseDbHelper.getTotalAmountSpent()
         val formattedString = resources.getString(R.string.total_amount_label, totalAmount.toString())
@@ -36,23 +43,27 @@ class MainActivity : Activity() {
 
     }
 
+    //addExpenses: Responsible for reading user input and updating the database
     fun addExpense(view: View) {
+        //finds the name of expense and retrieves it
         val nameEditText = findViewById<EditText>(R.id.expense_name)
         val name = nameEditText.text.toString()
-
+        //Null-check for the inputed name
         if (!name.isEmpty()) {
+            //finds user ammount and retrieves it
             val amountEditText = findViewById<EditText>(R.id.expense_amount)
             val amount = amountEditText.text.toString()
-
+            // Null-check for the amount
             if (!amount.isEmpty()) {
+                //converts the amount into double
                 val amount = amount.toDouble()
-
+                //sets and formats the date
                 val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
                 val date = dateFormat.format(Date())
-
+                //adds the expense based on the user's input
                 var expense = Expense(0, name, amount, date)
                 var expenseId = expenseDbHelper.insertExpense(expense)
-
+                //fixes id, adds expense into the database, and displays the uupdated list
                 if (expenseId != -1L) {
                     expense.id = expenseId
                     listAdapter?.add(expense)
@@ -61,7 +72,7 @@ class MainActivity : Activity() {
                     val totalAmount = expenseDbHelper.getTotalAmountSpent()
                     val formattedString = resources.getString(R.string.total_amount_label, totalAmount.toString())
                     totalAmountTextView.text = formattedString
-
+                    //resets the user input fields
                     nameEditText.setText("")
                     amountEditText.setText("")
                 }
@@ -72,6 +83,7 @@ class MainActivity : Activity() {
             return
         }
     }
+    //onDestroy: runs after the programs is closed
     override fun onDestroy() {
         super.onDestroy()
         expenseDbHelper.close()
